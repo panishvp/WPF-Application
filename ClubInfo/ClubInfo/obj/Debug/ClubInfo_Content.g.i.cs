083a5492856b87eb19b26cd136f837e8ba5866f0
Paneesh <PPrabhur@its.jnj.com>
2019-06-11 11:01:52 +0200

    Commiting Ui changes into repo

--- ClubInfo/ClubInfo/obj/Debug/ClubInfo_Content.g.i.cs
+++ ClubInfo/ClubInfo/obj/Debug/ClubInfo_Content.g.i.cs
@@ -8,25 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/1.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/10.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/11.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/12.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/13.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/14.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/15.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/16.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/17.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/18.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/19.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/2.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/20.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/3.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/4.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/5.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/6.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/7.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/8.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/9.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/logo.png")]
 
 
